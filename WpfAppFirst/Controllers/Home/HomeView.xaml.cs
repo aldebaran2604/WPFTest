dde0426271b20e4b7c506b4d02030f5ae44c599c
Luis Jose Padilla Benitez <lupadilla@bansi.com.mx>
2022-09-09 12:12:38 -0500

    First commit

---
+++ WpfAppFirst/Controllers/Home/HomeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfAppFirst.Controllers.Home
+{
+    /// <summary>
+    /// Lógica de interacción para HomeView.xaml
+    /// </summary>
+    public partial class HomeView : Window
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
